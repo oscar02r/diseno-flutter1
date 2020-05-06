f617dbfed9317965ba9064262366564d116509b5
oscar02r <oscary.ramirez@gmail.com>
2020-05-06 17:32:27 +0200

    Adding botonespage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/material.dart';
+
 import 'package:desino_flutter/src/page/basico_page.dart';
 import 'package:desino_flutter/src/page/scroll_page.dart';
-import 'package:flutter/material.dart';
+import 'package:desino_flutter/src/page/botones_page.dart';
 
  void main() => runApp(MyApp());
  
@@ -10,10 +12,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseno',
-      initialRoute: 'scroll',
+      initialRoute: 'botones',
       routes: {
         'basico' : (context) => BasicoPage(),
         'scroll' : (context) => ScrollPage(),
+        'botones' : (context) => BotonesPage(),
       },
       
     );
